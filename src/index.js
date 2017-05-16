a54c126c456273d6f4c93a626ae88a4add29c66a
Keith Wong <keith.wong3672@gmail.com>
2017-05-16 13:19:35 -0700

    Setup template for containers, components, route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 import App from './App';
@@ -11,7 +12,9 @@ import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
